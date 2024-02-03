8ba08e6a928ec08a69c31e370001c4bf7f6ae40e
Euller Amaral <eullergustavo63@gmail.com>
2024-02-03 17:39:31 -0300

    App configuration

---
+++ chuckNorrisFacts/ChuckNorrisFactsViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ChuckNorrisFactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
+
